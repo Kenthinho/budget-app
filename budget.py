d32727891cab02b98bb69f89d37462c3d395ca23
kenthinho <kenneth4justice@gmail.com>
2021-04-14 23:03:11 +0100

    modified my app

--- budget.py
+++ budget.py
@@ -1,15 +1,21 @@
 class Budget:
+
+    '''A budget app that contains a list of food,
+    entertainment and clothing in its category'''
+
     def __init__(self, category, balance = 0):
         self.category = category
         self.balance = balance
 
 
+    # A function that accepts money from its user
     def deposit(self):
         deposit_amount = int(input("Enter amount to deposit: \n"))
         self.balance += deposit_amount
         return f"Your new balance in your {self.category} budget is ${self.balance:,.2f}."
 
 
+    # A function for taking out fund from user's account
     def withdraw(self):
         withdraw_amount = int(input("How much would you like to withdraw? \n"))
         if withdraw_amount > self.balance:
@@ -19,6 +25,7 @@ class Budget:
             return f"Please take your cash. \n\n Your new balance is ${self.balance:,.2f}."
 
 
+    # A function for sending funds to different categories in our app
     def transfer(self):
         print(f"transfer from your {self.category} budget")
         transfer_fund = int(input("Enter amount to transfer: \n"))
